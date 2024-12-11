703b3c82c4508a66f71c6426f6d803870b3e2760
gkoz <genadi.kozarev+github@gmail.com>
2024-12-11 14:36:17 +0000

    displays when would the timer end

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let countdown;
 let timerDisplay = document.querySelector('.display__time-left');
+let endTime = document.querySelector('.display__end-time');
 
 const timer = seconds => {
     const now = Date.now();
     const then = now + seconds * 1000;
     // run this once because setInterval starts after 1 second
     displayTimeLeft(seconds);
+    displayEndTime(then);
     countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
         if (secondsLeft < 0) {
@@ -26,4 +28,13 @@ const displayTimeLeft = seconds => {
     document.title = display;
 };
 
-timer(63);
+const displayEndTime = timestamp => {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    endTime.textContent = `timer ends at ${hour}:${
+        minutes < 10 ? '0' : ''
+    }${minutes}`;
+};
+
+timer(1620);
